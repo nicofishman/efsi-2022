339db32fa6c271b86b5c039fd5d39c58bf14ae97
antohausa <nicourifishman@gmail.com>
2022-03-31 08:03:11 -0300

    [EFSI] - TP2 Follow

--- EcmaScript 6/TP2/script.js
+++ EcmaScript 6/TP2/script.js
@@ -1,10 +1,21 @@
+document.querySelector('#tareaInput').addEventListener('keyup', (e) => {
+    if(e.keyCode === 13){
+        if(e.target.value !== ''){
+            agregarTarea();
+        }
+    }
+});
 let tareas = [];
 
 function refrescarTareas() {
     let lista = document.querySelector("#tareas");
     lista.innerHTML = "";
+    let clases = 'tarea';
     tareas.forEach((tarea) => {
-        lista.innerHTML += `<div class="tarea" id=${tarea.id}>
+        if(tarea.completed){
+            clases = 'tarea completada'
+        }
+        lista.innerHTML += `<div class=${clases} id=${tarea.id}>
         <input type="checkbox" onclick="completarTarea(${tarea.id})">
         <p class="textoTarea">${tarea.titulo}</p>
         </div>`
@@ -13,13 +24,20 @@ function refrescarTareas() {
 
 
 function agregarTarea(){
-    let tareaNombre = document.querySelector("#tareaInput").value;
-    let tarea = new Tarea(tareaNombre);
+    let tareaInput = document.querySelector("#tareaInput");
+    let tarea = new Tarea(tareaInput.value);
     tareas.push(tarea);
     console.log(tareas);
     refrescarTareas();
+    tareaInput.value = '';
 }
 
 function completarTarea(id){
-
+    let tarea = tareas.find((tarea) => {
+        return tarea.id === id;
+    });
+    tarea.marcar();
+    refrescarTareas();
 }
+
+
