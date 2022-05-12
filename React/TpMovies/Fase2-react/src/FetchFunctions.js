7c256b2cf575342325dc77ccc88ba46a896d4134
nicofishman <74215078+nicofishman@users.noreply.github.com>
2022-05-12 10:36:08 -0300

    Api en Español

--- React/TpMovies/Fase2-react/src/FetchFunctions.js
+++ React/TpMovies/Fase2-react/src/FetchFunctions.js
@@ -1,13 +1,13 @@
 const ApiKey = import.meta.env.VITE_API_KEY
 
 export const movieById = async (id) => {
-    const response = await fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${ApiKey}&language=en-US`)
+    const response = await fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${ApiKey}&language=es-ES`)
     const data = await response.json()
     return data
 }
 
 export const castById = async (id) => {
-    const response = await fetch(`https://api.themoviedb.org/3/movie/${id}/credits?api_key=${ApiKey}&language=en-US`)
+    const response = await fetch(`https://api.themoviedb.org/3/movie/${id}/credits?api_key=${ApiKey}&language=es-ES`)
     const data = await response.json()
     return data
 }
