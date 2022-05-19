ca74cb06d1d8718b576acbd467009461b9ef82c0
nicofishman <nicourifishman@gmail.com>
2022-05-19 09:44:38 -0300

    Axios

--- React/TpMovies/Fase2-react/src/FetchFunctions.js
+++ React/TpMovies/Fase2-react/src/FetchFunctions.js
@@ -1,14 +1,14 @@
+import axios from 'axios';
+
 const ApiKey = import.meta.env.VITE_API_KEY
 
 export const movieById = async (id, location = 'movie', filters = []) => {
-    console.log()
-    const response = await fetch(`https://api.themoviedb.org/3/${location}/${id}?api_key=${ApiKey}&language=es-ES${(filters) && filters.map( filter => filter )}`)
-    const data = await response.json()
-    return data
+    console.log(`https://api.themoviedb.org/3/${location}/${id}?api_key=${ApiKey}&language=es-ES${(filters) && filters.map(filter => filter)}`)
+    const response = await axios.get(`https://api.themoviedb.org/3/${location}/${id}?api_key=${ApiKey}&language=es-ES${(filters) && filters.map(filter => filter)}`)
+    return response.data
 }
 
 export const castById = async (id) => {
-    const response = await fetch(`https://api.themoviedb.org/3/movie/${id}/credits?api_key=${ApiKey}&language=es-ES`)
-    const data = await response.json()
-    return data
+    const response = await axios.get(`https://api.themoviedb.org/3/movie/${id}/credits?api_key=${ApiKey}&language=es-ES`)
+    return response.data
 }
--- React/TpMovies/Fase2-react/src/Routes/Index.jsx
+++ React/TpMovies/Fase2-react/src/Routes/Index.jsx
@@ -8,11 +8,11 @@ import { movieById } from '../FetchFunctions'
 
 
 function App() {
-    const mostSearchedFilters = [{name: 'En Streaming', type: 'popular', location: 'movie'}, {name: 'En Televisión', type: 'popular', location: 'tv' },{name: 'En Alquiler', type: 'movie', location: 'discover', filters: ['&with_watch_monetization_types=rent'] }, {name: 'En Cines', type: 'now_playing', location: 'movie'}]
-    const topRatedFilters = [{name: 'Hoy'}, {name: 'Esta Semana'}]
+    const mostSearchedFilters = [{ name: 'En Streaming', type: 'popular', location: 'movie' }, { name: 'En Televisión', type: 'popular', location: 'tv' }, { name: 'En Alquiler', type: 'movie', location: 'discover', filters: ['&with_watch_monetization_types=rent'] }, { name: 'En Cines', type: 'now_playing', location: 'movie' }]
+    const topRatedFilters = [{ name: 'Hoy' }, { name: 'Esta Semana' }]
 
 
-    const [mostSearchedMoviesTyes, setMostSearchedMoviesTyes] = useState({name: 'En Streaming', type: 'popular', location: 'movie', filters: []})
+    const [mostSearchedMoviesTyes, setMostSearchedMoviesTyes] = useState({ name: 'En Streaming', type: 'popular', location: 'movie', filters: [] })
 
     const [mostSearchedMovies, setMostSearchedMovies] = useState([])
     const [topRatedMovies, setTopRatedMovies] = useState([])
@@ -20,8 +20,7 @@ function App() {
 
     useEffect(() => {
         const fetchMovies = async () => {
-            
-            const mostSearched = await movieById(mostSearchedMoviesTyes.name, mostSearchedMoviesTyes.location, mostSearchedMoviesTyes.filters)
+            const mostSearched = await movieById(mostSearchedMoviesTyes.type, mostSearchedMoviesTyes.location, mostSearchedMoviesTyes.filters)
 
             const topRated = await movieById('top_rated')
             const upcoming = await movieById('upcoming')
