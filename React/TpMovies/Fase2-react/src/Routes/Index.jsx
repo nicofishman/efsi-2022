f9551aa710acfb42488c619cc48b3fea43134275
Jaco <Jaco>
2022-05-19 09:07:46 -0300

    filtros

--- React/TpMovies/Fase2-react/src/Routes/Index.jsx
+++ React/TpMovies/Fase2-react/src/Routes/Index.jsx
@@ -8,15 +8,21 @@ import { movieById } from '../FetchFunctions'
 
 
 function App() {
-    const mostSearchedFilters = ['En Streaming', 'En Televisión', 'En Alquiler', 'En Cines']
-    const topRatedFilters = ['Hoy', 'Esta Semana']
+    const mostSearchedFilters = [{name: 'En Streaming', type: 'popular', location: 'movie'}, {name: 'En Televisión', type: 'popular', location: 'tv' },{name: 'En Alquiler', type: 'movie', location: 'discover', filters: ['&with_watch_monetization_types=rent'] }, {name: 'En Cines', type: 'now_playing', location: 'movie'}]
+    const topRatedFilters = [{name: 'Hoy'}, {name: 'Esta Semana'}]
+
+
+    const [mostSearchedMoviesTyes, setMostSearchedMoviesTyes] = useState({name: 'En Streaming', type: 'popular', location: 'movie', filters: []})
+
     const [mostSearchedMovies, setMostSearchedMovies] = useState([])
     const [topRatedMovies, setTopRatedMovies] = useState([])
     const [upcomingMovies, setUpcomingMovies] = useState([])
 
     useEffect(() => {
         const fetchMovies = async () => {
-            const mostSearched = await movieById('popular')
+            
+            const mostSearched = await movieById(mostSearchedMoviesTyes.name, mostSearchedMoviesTyes.location, mostSearchedMoviesTyes.filters)
+
             const topRated = await movieById('top_rated')
             const upcoming = await movieById('upcoming')
             console.log(mostSearched.results)
@@ -44,7 +50,7 @@ function App() {
             <SearchMovieInput />
             <MovieSection title='Lo Más Buscado' movies={mostSearchedMovies} filters={mostSearchedFilters}></MovieSection>
             <MovieSection title='Lo Mejorcito' movies={topRatedMovies} filters={topRatedFilters}></MovieSection>
-            <MovieSection title='Lo que se viene' movies={upcomingMovies} filters={[]}></MovieSection>
+            <MovieSection title='Lo que se viene' movies={upcomingMovies} filters={null}></MovieSection>
         </Box>
     )
 }
